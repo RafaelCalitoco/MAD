8d41a1a57d78842e11811f1a07cc8e4518fa185e
RafaelCalitoco <rafaelcalitoco@gmail.com>
2016-11-07 17:05:03 +0000

    Add files via upload

---
+++ AlunoDao.cs
@@ -0,0 +1,53 @@
+﻿using Escola_conducao.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Escola_conducao.Dao
+{
+    public class AlunoDao
+    {
+        private Aluno aluno;
+        private List<Aluno> alunos;
+        private string conexao;
+
+        public AlunoDao()
+        { }
+
+        public void inserir(Aluno aluno)
+        {
+            conexao = new Conexao().getConexao();
+            MySqlConnection sqlc = new MySqlConnection(conexao);
+            sqlc.Open();
+            MySqlCommand comando = new MySqlCommand("INSERT INTO aluno(Nome, BI, Telefone, Email) values ('" + aluno.getNome() + "', '" + aluno.getBI() + "', '" + aluno.getTelefone() + "', '" + aluno.getEmail() + "') ", sqlc);
+            int a = comando.ExecuteNonQuery();
+            // Fecha a conexão
+            sqlc.Close();
+ 
+
+        }
+
+        public void update(Aluno aluno)
+        {
+
+        }
+
+        public void delete(Aluno aluno)
+        {
+
+        }
+
+        public Aluno getAluno()
+        {
+            return aluno;
+        }
+
+        public List<Aluno> getAlunos()
+        {
+            return alunos;
+        }
+    }
+}
